5a7a372d0dac6f3a89ceb9a1f471fe79a5dda775
Gostin <88909096+GreatGostin@users.noreply.github.com>
2021-12-17 21:37:23 +0100

    Update main.c
    -add: simple Min Max Detection
    -mod: Made a lot of global varibales into local or Structs.
    -mod:  some Variables are made static

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-
 #include <msp430.h>
 #include "PCD8544.h"
 #include <math.h>
+#include <stdio.h>
 
 //LDC PIN DEFINES
 #define LCD5110_SCLK_PIN            BIT5
@@ -29,28 +29,42 @@ unsigned int ADCValue[8];//
 unsigned int StartStepA   = 0;
 unsigned int StartStepB   = 0;
 
-unsigned int HTresholdRed = 0xFFFF; //Initial Value 0xFFFF
-unsigned int LTresholdRed = 0;
-unsigned int HTresholdInfra = 0xFFFF;
-unsigned int LTresholdInfra = 0;
+unsigned int MaxIntensity = 10;
 
-//Variables for Processing ADC Values;
-unsigned long rms_AC_Red   = 0;
-unsigned long mean_DC_Red   = 0;
-unsigned long rms_AC_Infra = 0;
-unsigned long mean_DC_Infra = 0;
-unsigned int MeanTemp = 0;
-unsigned int VoltageLevel = 0;
+static unsigned int SamplesBPM = 250; //Per 1 sec we take roughly 250 samples of the red LED
+static unsigned int NumOfMaxTaken = 4;
+static unsigned int SPO2Taken = 2; //Right Shift
 
-unsigned int maxIntensity = 10;
 
-unsigned int MaxACRed   = 0;
-unsigned int MinACRed   = 0;
+static unsigned int HTresholdRed = 0xFFFF;
+static unsigned int LTresholdRed = 0;
+static unsigned int HTresholdInfra = 0xFFFF;
+static unsigned int LTresholdInfra = 0;
 
-unsigned int LCDBattFlag = 0;
 
-unsigned int SPO2Level = 0;
-unsigned int MeanSPO2 = 0;
+
+
+typedef enum TagDirection
+{
+    Rising = 0,
+    Falling
+}Direction;
+
+typedef struct TagMinMax
+{
+    unsigned int Max;
+    unsigned int Min;
+    unsigned int LastValue[5];
+    unsigned int NumOfPeaks;
+    unsigned int I;
+    unsigned int Dir;
+}MinMax;
+
+typedef struct TagLEDValues
+{
+    unsigned long MeanDC;
+    unsigned long RmsAC;
+}LEDValues;
 
 
 //Set All Pins to Configure the LDC
@@ -62,45 +76,62 @@ void GetDiodeADC(unsigned int chann);
 
 //Take the ADC Value of the NTC and converts it to the equivalent Temperature.
 //Used for slight Correction of LEDS;
-void GetTemp(void);
+void GetTemp(unsigned int* meanTemp);
 
 //Take the ADV Value of the LiPo Battery.
 //Checks for it to be between 3.7 to 3.0 ??
 //Sets Flags to Change the Battery Sign on the LCD
-void GetBatteryVoltage(void);
+void GetBatteryVoltage(unsigned int* lcdBattFlag);
 
 
 //Checks the Threshold for either red or infrared.
 //Tries to keep it in a range by changing the PWM that controls the Intensity
 void CheckLEDIntensity(void);
 
-int CalculateSP02(void);
-int CaluclateRPM(int samples);
-
+void CalculateSP02(unsigned int* meanSPO2, unsigned long rms_AC_RED, unsigned long rms_AC_Infra);
+int CaluclateBPM(int samples);
+void LCDWriteStatusValues(unsigned int SP02, unsigned int bpm, unsigned int battStatus);
 
 
 void main(void) {
 
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
-    /*  Default settings after reset:
-    *
-    *   Source of the Main system clock (MCLK) and sub-main system clock (SMCLK) is
-    *   Internal Digitally Controlled Oscillator Clock (DCO).
-    *   MCLK is about 1 MHz.
-    *   VLO is about 32 kHz.
-    *
-    *
-    */
-    //Variables used for the normal sequence
+    MinMax maxDetect;
+    memset( &maxDetect, 0, sizeof(maxDetect));
+    maxDetect.Dir = Falling;
+
+    unsigned int sampleDC = 0;
+    unsigned int sampleAC = 0;
+
     unsigned int i = 0;
     unsigned int samplesHBeat = 0;
     unsigned int takeBattery  = 0;
     unsigned int updLCD    = 0;
     unsigned int bpm =0;
+    unsigned int calcBPM = 0;
+    unsigned int firstIterationOver = 0;
+    unsigned int maxCheck = 0;
+    unsigned int minCheck = 0;
+    unsigned int dataValid = 0;
 
+    unsigned int maxACRed   = 0;
+    unsigned int minACRed   = 0;
 
+    LEDValues red;
+    red.MeanDC = 0;
+    red.RmsAC = 0;
+    LEDValues infra;
+    infra.MeanDC = 0;
+    infra.RmsAC = 0;
 
+    unsigned int meanTemp = 0;
+    unsigned int voltageLevel = 0;
+
+    unsigned int LCDBattFlag = 0;
+
+    unsigned int SPO2Level = 0;
+    unsigned int meanSPO2 = 0;
 
     //Configuration for ADC Pin start on DC for RED (A3, P1.3)
     //Later each ADC Pin (A1, A3, A10, A11) is configured and triggered manually
@@ -119,25 +150,21 @@ void main(void) {
     //ADCIE |= ADCIE0;
 
     InitLCDPins();
-
     __delay_cycles(300000); // we have to wait a bit for the LCD to boot up.
     initLCD();
-
     clearLCD();
 
     __delay_cycles(500000); //So that we come from a fres boot the Screen will be empty and doesn't accidentally contain old Information
 
-
     //Use PIn 5.0 and 5.1 for switching the LEDs
-
     P5DIR |= BIT0 | BIT1;
     P5SEL0 |= BIT0 | BIT1;
 
-    TB2CCR0  = 1000-1;          //Every 1 ms  the LED is already on for 110 us and will continue for another 110 us
+    TB2CCR0  = 2000-1;          //Every 1 ms  the LED is already on for 110 us and will continue for another 110 us
     TB2CCTL1 |= OUTMOD_2;    //TB2CCR1 toggle/set
-    TB2CCR1  = 110-1;         //0..110us , 1.89ms ... 2ms
+    TB2CCR1  = 500-1;         //0..110us , 1.89ms ... 2ms
     TB2CCTL2 |= OUTMOD_6;    //TBCCR2 reset/set
-    TB2CCR2  = 890-1;         //0.890 ms - 1.11 ms
+    TB2CCR2  = 1500-1;         //0.890 ms - 1.11 ms
     TB2CTL   |= TBSSEL_2 | MC_3 |TBCLR; // SMCLK, Up-Down-Mode, Interrupt Enable on Max Value and Zero
     TB2CTL   |= TBIE;
     TB2CCTL0 |= CCIE; //Enable Interrupt when CCR0 is reached.
@@ -148,14 +175,12 @@ void main(void) {
     P6DIR  |= BIT3 | BIT4;
     P6SEL0 |= BIT3 | BIT4;
 
-
-    TB3CCR0 = maxIntensity;
+    TB3CCR0 = MaxIntensity;
     TB3CCTL4 |= OUTMOD_7;
     TB3CCTL5 |= OUTMOD_7;
-    TB3CCR4   = 4;
-    TB3CCR5   = 4;
+    TB3CCR4   = MaxIntensity>>1;
+    TB3CCR5   = MaxIntensity>>1;
     TB3CTL   |= TBSSEL__SMCLK  | MC__UP | TBCLR; //Up-Mode 1 MHz
-
 
     PM5CTL0 &= ~LOCKLPM5; //Without this the pins won't be configured in Hardware.
     TB2CTL &= ~TBIFG;
@@ -184,41 +209,52 @@ void main(void) {
         {
             GetDiodeADC(DCRED);
 
-            //Take RMS of AC and Mean of
-            rms_AC_Red = (ADCValue[ACRED]*ADCValue[ACRED]) >> 9; //Div by 9 makes it easier
-            mean_DC_Red = ADCValue[DCRED] >> 9;
+            //Take RMS of AC and Mean of DC
+            sampleDC = ADCValue[DCRED];
+            sampleAC = ADCValue[ACRED];
 
-            //Wait a bit more (like 20us) to start the ADC for both Diodes off
-            __delay_cycles(20);
+            red.RmsAC += (long)(sampleAC * sampleAC) >> 8; //Div by 8 makes it easier
+            red.MeanDC += (long)sampleDC >> 8;
 
-            GetTemp();
+            GetTemp(&meanTemp);
 
+            if(!firstIterationOver && dataValid)
+            {
+
+                samplesHBeat++;
+                MinMaxDetection(&maxDetect, sampleAC);
+
+                if(maxDetect.NumOfPeaks == 5)
+                {
+                    bpm = CaluclateBPM(samplesHBeat);
+                    samplesHBeat = 0;
+                    maxDetect.NumOfPeaks = 0;
+                }
+            }
             GetDiodeADC(DCOFF);
-
-
 
         }
         else if(StartStepB)
         {
             GetDiodeADC(DCINFRA);
-            rms_AC_Infra += (ADCValue[ACINFRA]*ADCValue[ACINFRA]) >> 9; //Div by 9 makes it easier
-            mean_DC_Infra += ADCValue[DCINFRA] >> 9;
+            sampleDC = ADCValue[DCINFRA];
+            sampleAC = ADCValue[ACINFRA];
+
+            infra.RmsAC  += (long)(sampleAC*sampleAC) >> 8; //Div by 8 makes it easier
+            infra.MeanDC += (long)sampleDC >> 8;
 
             i++;
-
             if(i == 1000)
             {
-                CalculateSP02();
+                CalculateSP02(&meanSPO2, red.RmsAC, infra.RmsAC);
 
-
+                LCDWriteStatusValues(meanSPO2, bpm, LCDBattFlag);
+                i= 0;
                 //Send the Data via SPI.
             }
-
+            firstIterationOver = 1;
         }
-
     }
-
-
 } // eof main
 
 
@@ -239,29 +275,55 @@ __interrupt void TIMER2_B1_ISR(void)
 
 
 
-//Takes around 80 (80 us) for the entire function
+//Check the maximum
+void MinMaxDetection(MinMax* detect, unsigned int sample)
+{
+    unsigned int check;
+    unsigned int* i = &detect->I;
+    detect->LastValue[*i] = sample;
+    *i++;
+
+    if(*i==4)
+    {
+        check = detect->LastValue[0];
+        if(check > detect->LastValue[1] && check > detect->LastValue[2] && check > detect->LastValue[3] && check > detect->LastValue[4] && detect->Dir == Rising)
+        {
+            detect->NumOfPeaks++;
+            detect->Dir = Falling;
+        }
+        else if(check < detect->LastValue[1] && check < detect->LastValue[2] && check < detect->LastValue[3] && check < detect->LastValue[4])
+        {
+            detect->Dir = Rising;
+        }
+        *i = 0;
+
+    }
+
+}
+
+
 //Using Active Polling because the combination of Low Power Mode and Interrupt  isn't working properly
-void GetDiodeADC(unsigned int chann)
+void GetDiodeADC(unsigned int channel)
 {
     ADCCTL0  &= ~ADCENC;
     ADCMCTL0 |= ADCINCH_3;
     ADCCTL0  |= ADCENC | ADCSC;
 
     while(ADCCTL1 & ADCBUSY);
-    ADCValue[chann] = ADCMEM0;
+    ADCValue[channel] = ADCMEM0;
 
     ADCCTL0  &= ~ADCENC;
     ADCMCTL0 |= ADCINCH_10;
     ADCCTL0  |= ADCENC | ADCSC;
 
-    chann= chann+1;
+    channel++;
 
     while(ADCCTL1 & ADCBUSY);
-    ADCValue[chann] = ADCMEM0;
+    ADCValue[channel] = ADCMEM0;
 
 }
 
-void GetTemp(void)
+void GetTemp(unsigned int* meanTemp)
 {
     ADCCTL0  &= ~ADCENC;
     ADCMCTL0 |= ADCINCH_11;
@@ -270,39 +332,40 @@ void GetTemp(void)
     while(ADCCTL1 & ADCBUSY);
     ADCValue[TEMP] = ADCMEM0;
 
-    MeanTemp =+ (ADCValue[TEMP] >> 9);
+    *meanTemp =+ (ADCValue[TEMP] >> 8);
 }
-void GetBatteryVoltage(void)
+void GetBatteryVoltage(unsigned int* lcdBattFlag)
 {
+    unsigned int voltageLevel = 0;
     ADCCTL0  &= ~ADCENC;
     ADCMCTL0 |= ADCINCH_1;
     ADCCTL0  |= ADCENC | ADCSC;
 
     while(ADCCTL1 & ADCBUSY);
 
-    VoltageLevel = ADCMEM0;
+    voltageLevel = ADCMEM0;
 
     //Based on the battery level, Change a Flag for The LCD
 
-    if(VoltageLevel >= 0xF000)
+    if(voltageLevel >= 0xF000)
     {
-        LCDBattFlag = 4; // Full
+        *lcdBattFlag = BATTERY_FULL; // Full
     }
-    else if(VoltageLevel >= 0x8000 && VoltageLevel < 0xF000)
+    else if(voltageLevel >= 0x8000 && voltageLevel < 0xF000)
     {
-        LCDBattFlag = 3; // One line missing (~ 75%)
+        *lcdBattFlag = BATTERY_75; // One line missing (~ 75%)
     }
-    else if(VoltageLevel >= 0x4000 && VoltageLevel < 0x8000)
+    else if(voltageLevel >= 0x4000 && voltageLevel < 0x8000)
     {
-        LCDBattFlag = 2; // Two lines missing (~ 50%)
+        *lcdBattFlag = BATTERY_50; // Two lines missing (~ 50%)
     }
-    else if(VoltageLevel >= 0x2000 && VoltageLevel < 0x4000)
+    else if(voltageLevel >= 0x2000 && voltageLevel < 0x4000)
     {
-        LCDBattFlag = 1; // Three lines missing (~ 25$)
+        *lcdBattFlag = BATTERY_25; // Three lines missing (~ 25%)
     }
     else
     {
-        LCDBattFlag ^= BIT0; // Blinking
+        *lcdBattFlag ^= BIT0; // Blinking, BATTERY_10 (0x0)
     }
 
 }
@@ -310,6 +373,8 @@ void GetBatteryVoltage(void)
 
 //We will maybe every 100 ms Check for the Intensity
 //Maybe even more. Its need to be evaluated during testing.
+//
+//We need to differentiate when we pull out the finger. This will lead to a maximum, Minum
 void CheckLEDIntensity(void)
 {
     //Disable both PWM
@@ -318,9 +383,9 @@ void CheckLEDIntensity(void)
     if(ADCValue[ACRED] < LTresholdRed)
     {
         TB3CCR4++;
-        if(TB3CCR4 >= maxIntensity)
+        if(TB3CCR4 >= MaxIntensity)
         {
-            TB3CCR4 = maxIntensity;
+            TB3CCR4 = MaxIntensity;
         }
     }
     else if(ADCValue[ACRED] < HTresholdRed)
@@ -335,9 +400,9 @@ void CheckLEDIntensity(void)
     if(ADCValue[ACRED] < LTresholdInfra)
     {
         TB3CCR5++;
-        if(TB3CCR5 >= maxIntensity)
+        if(TB3CCR5 >= MaxIntensity)
         {
-            TB3CCR5 = maxIntensity;
+            TB3CCR5 = MaxIntensity;
         }
     }
     else if(ADCValue[ACRED] < HTresholdInfra)
@@ -361,33 +426,31 @@ void CheckLEDIntensity(void)
     TB3CTL |= MC__UP;
 }
 
-int CalculateSP02(void)
+void CalculateSP02(unsigned int* meanSPO2, unsigned long rms_AC_Red, unsigned long rms_AC_Infra)
 {
     unsigned int R;
+    unsigned int SPO2Level;
     //  Possibility 1
     //  R = ((rms_AC_Red / mean_DC_Red)/(rms_AC_Infra/mean_DC_Infra) -0.5) *100; //We have to subtract 0.5 so that we can Access the Array
     //  Possibiltiy 2
-        R = log(sqrt(rms_AC_Red))/log(sqrt(rms_AC_Infra)); //when DC_Red == DC_Infra
+    R = log(sqrt(rms_AC_Red))/log(sqrt(rms_AC_Infra)); //when DC_Red =~ DC_Infra
 
     SPO2Level =  1;
     //SP02Level = SP02[R];
 
-    MeanSPO2 += SPO2Level >> 2; //We take an Average of 4 Values to Calculate the Sp02 Level
-
-    return MeanSPO2;
+    *meanSPO2 += (SPO2Level >> SPO2Taken); //We take an Average of 4 Values to Calculate the Sp02 Level
 }
 
-int CaluclateRPM(int samples)
+int CaluclateBPM(int samples)
 {
     unsigned int bpm;
-    bpm = (500* 60)/(samples/4);
+    bpm = (SamplesBPM* 60)/(samples/NumOfMaxTaken);
     return bpm;
 }
 
 
 void InitLCDPins(void)
 {
-
     P1SEL0 |= LCD5110_DN_PIN | LCD5110_SCLK_PIN;
     P1OUT &= ~BIT0;
 
@@ -402,8 +465,13 @@ void InitLCDPins(void)
     UCA0CTLW0 |= UCSSEL_3; // SMCLK
     UCA0BRW   |= 0; //1:1
 
-
     UCA0CTLW0 &= ~UCSWRST;
+}
+
+//Writes everything needed that needs to be updated
+void LCDWriteStatusValues(unsigned int SP02, unsigned int bpm, unsigned int battStatus)
+{
+
 
 }
 
@@ -480,5 +548,4 @@ void clearBank(unsigned char bank) {
     }
     setAddr(0, bank);
 }
-
 
